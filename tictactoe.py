12ab2b7eabd68e546cfb4bd5a6c44b8d101c366c
omikron99 <omikronyt@gmail.com>
2020-04-27 18:51:16 +0200

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,160 @@
+import random, os
+
+#funkcja odpowiedzialna za czyszczenie konsoli
+clear = lambda: os.system("cls")
+
+symbol = ['O', "X"]
+players = {}
+
+#Tablica przechowujaca ruchy
+game = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
+]
+
+# True - koniec gry, ktoś wygrał lub remis, False - nikt nie wygrał
+Win = False
+
+#Rysuje plansze
+def drawGameBoard(arrayGame):
+    print("-------")
+    print("|%s|%s|%s|" % (arrayGame[0][0], arrayGame[0][1], arrayGame[0][2]))
+    print("-------")
+    print("|%s|%s|%s|" % (arrayGame[1][0], arrayGame[1][1], arrayGame[1][2]))
+    print("-------")
+    print("|%s|%s|%s|" % (arrayGame[2][0], arrayGame[2][1], arrayGame[2][2]))
+    print("-------")
+
+#Jeżeli w każdym wierszu nie ma wolnych miejsc a nikt nie wygrał to remis
+def checkDraw(arrayGame):
+    count = 0
+    for i in arrayGame:
+        if not ' ' in i:
+            count += 1
+    if count == 3:
+        return True
+    else:
+        return False
+
+#sprawdza czy ktoś wygrał w poziomie
+def horizontallyWin(arrayGame):
+    for i in range(len(arrayGame)):
+        if len(set(arrayGame[i])) == 1 and arrayGame[i][0] != ' ':
+            return True
+    return False
+
+#sprawdza czy ktoś wygrał na krzyż
+def crossWin(arrayGame):
+    if arrayGame[1][1] == ' ':
+        return False
+    else:
+        if arrayGame[0][0] == arrayGame[1][1] == arrayGame[2][2]:
+            return True
+        elif arrayGame[0][2] == arrayGame[1][1] == arrayGame[2][0]:
+            return True
+
+#sprawdza czy ktoś wygrał w pionie
+def verticalWin(arrayGame):
+    if arrayGame[0][0] == arrayGame[1][0] == arrayGame[2][0] and arrayGame[0][0] != ' ':
+        return True
+    elif arrayGame[0][1] == arrayGame[1][1] == arrayGame[2][1] and arrayGame[0][1] != ' ':
+        return True
+    elif arrayGame[0][2] == arrayGame[1][2] == arrayGame[2][2] and arrayGame[0][2] != ' ':
+        return True
+    else:
+        return False
+
+#losuje losowy symbol dla gracza po czym wyrzuca z tablicy wylosowany symbol
+def randomSymbol(symbol):
+    randomSymbol = random.choice(symbol)
+    indexSymbol = symbol.index(randomSymbol)
+    return symbol.pop(indexSymbol)
+
+#dodaje gracza do rozgrywki
+def addPlayer(player, arrayPlayer):
+    symbolPlayer = randomSymbol(symbol)
+    print("Cześć %s twój wylosowany symbol to: %s\n" % (player, symbolPlayer))
+    arrayPlayer[player] = symbolPlayer
+
+#funkcja odpowiedzialna za zapisanie ruchu gracza do tablicy
+def drawMove(player, arrayPlayer, arrayGame):
+    while True:
+        cordinate = input("Podaj wspolrzedne x oraz y (x y): ")
+        plitCordinate = cordinate.split(' ')
+        try:
+            x = int(splitCordinate[0])
+            y = int(splitCordinate[1])
+            if arrayGame[x][y] != ' ':
+                print("Bład: To pole jest zajęte!")
+                continue
+            else:
+                arrayGame[x][y] = arrayPlayer[player]
+                break
+        except NameError:
+            print("Program osbsługuje tylko wartości liczbowe i mogą przyjmować one tylko wartośći od 0 do 2")
+        except:
+            print("Nieznany błąd: ")
+
+#funkcja sprawdza czy padła wygrana lub remis
+def checkCollision(player, arrayGame):
+    if horizontallyWin(arrayGame):
+        print("Gracz %s wygrał!" % (player))
+        input("Aby zakończyć kliknij klawisz Enter")
+        return True
+    elif verticalWin(arrayGame):
+        print("Gracz %s wygrał!" % (player))
+        input("Aby zakończyć kliknij klawisz Enter")
+        return True
+    elif crossWin(arrayGame):
+        print("Gracz %s wygrał!" % (player))
+        input("Aby zakończyć kliknij klawisz Enter")
+        return True
+    elif checkDraw(arrayGame):
+        print("Remis!")
+        input("Aby zakończyć kliknij klawisz Enter")
+        return True
+
+
+name = input("Wprowadź nazwę pierwszego gracza: ")
+addPlayer(name, players)
+
+player_1_Name = name
+
+while True:
+    name = input("Wprowadź nazwę drugiego gracza: ")
+    if player_1_Name != name:
+        addPlayer(name, players)
+        break
+    else:
+        print("Nazwa gracza drugiego musi być inna niż gracza pierwszego")
+
+playerNameList = list(players.keys()) #lista która przechowuje klucze tablicy
+player1 = playerNameList[0] #pobranie klucza jako nazwy gracza
+player2 = playerNameList[1] #pobranie klucza jako nazwy gracza
+
+drawGameBoard(game)
+
+#Pętla się wykonuje dopóki ktoś nie wygra Win = true
+while not Win:
+    print("\n--= Ruch gracza %s =--" % (player1))
+    drawMove(player1, players, game)
+    clear()
+    drawGameBoard(game)
+
+    Win = checkCollision(player1, game)
+    if Win:
+        continue
+
+    print("\n--= Ruch gracza %s =--" % (player2))
+    drawMove(player2, players, game)
+    clear()
+    drawGameBoard(game)
+
+    Win = checkCollision(player2, game)
+    if Win:
+        continue
+
+
+
+
